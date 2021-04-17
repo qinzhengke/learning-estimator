e10285dffb7f7f87062d6f73fa4515292788ab4f
qinzhengke <qinzhengke>
2021-04-18 03:13:07 +0800

    [ING]Working on simulator for particle filter.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # learning-estimator
 Learning estimators by examples!
+
+## Depenencies
+
+```
+sudo apt install libx11-dev libxrandr-dev libxinerama libxcursor libxi libgl1-mesa-dev
+```
+
--- particle_filter/main.cc
+++ particle_filter/main.cc
@@ -20,47 +20,72 @@ class Robot{
 public:
 
     Point pos;
-    Point p0,p1={1,0},p2;
+    float a0_,a1_=0,a2_=M_PI/2.0;
+    Point q0_,q1_={cos(a1_),sin(a1_)};
     default_random_engine rand_;
 
     Robot(){
-        plan();
-        plan();
-        plan();
     }
 
     tuple<vector<float>,vector<float>> currPlanLine(){
         vector<float>x,y;
         const int n = 100;
+        Point p0 = {cos(a0_),sin(a0_)}, p1={cos(a1_),sin(a1_)};
         float dx = (p1.x - p0.x)/(float)n, dy = (p1.y - p0.y)/(float)n;
         for(int i=0; i<n+1; i++){
             x.push_back(p0.x+dx*(float)i);
             y.push_back(p0.y+dy*(float)i);
+        }
+
+        // Point p2 = {cos(a2_), sin(a2_)};
+        // dx = (p2.x-p1.x)/(float)(n), dy = (p2.y-p1.y)/(float)n;
+
+        // for(int i=0; i<n+1; i++){
+        //     x.push_back(p1.x+dx*(float)i);
+        //     y.push_back(p1.y+dy*(float)i);
+        // }
+        return tuple<vector<float>,vector<float>>(x,y);
+    }
+
+    tuple<vector<float>,vector<float>> currPlanCurve(){
+        vector<float>x,y;
+        const int n=100;
+        Point p0 = {cos(a0_),sin(a0_)}, p1={cos(a1_),sin(a1_)};
+        Point q2 = {p0.x+(p1.x-p0.x)*0.2f, p0.y+(p1.y-p0.y)*0.2f};
+        float dx = (q2.x - q0_.x)/(float)n, dy = (q2.y - q0_.y)/(float)n;
+        for(int i=0; i<n; i++){
+            float t = (float)i/(float)n;
+            x.push_back(q0_.x + dx*(float)i);
+            y.push_back(q0_.y + dy*(float)i);
+        }
+
+        for(int i=0; i<n; i++){
+            float t = (float)i/(float)n;
+            x.push_back((1.0-t)*(1.0-t)*q2.x + 2.0*t*(1.0-t)*p1.x + t*t*q1_.x);
+            y.push_back((1.0-t)*(1.0-t)*q2.y + 2.0*t*(1.0-t)*p1.y + t*t*q1_.y);
         }
         return tuple<vector<float>,vector<float>>(x,y);
     }
 
     Point move(){
 
+        Point pt;
+        return pt;
     }
 
-    Point plan_p1(Point p0){
-        float d = M_PI/180.0 * 10.0;
-        float a = atan2(p0.y, p0.x);
-        float a_min = a+d, a_max=(a-d+2*M_PI);
-
-        uniform_real_distribution<float> dist(a_min,a_max);
-        float b = dist(rand_);
-        return Point({cos(b), sin(b)});
-    }
-
-    Point plan_p2(Point p0, Point p1){
-
-    }
 
     void plan(){
-        p0 = p1;
-        p1 = plan_p1(p0);
+        a0_ = a1_;
+        a1_ = a2_;
+        float d = M_PI/180.0 * 60.0;
+        float a2_min =  a1_ + d, a2_max = a1_ + M_PI;
+        uniform_real_distribution<float> dist2(a2_min,a2_max);
+        a2_ = dist2(rand_);
+
+        q0_ = q1_;
+        Point p0 = {cos(a0_),sin(a0_)}, p1={cos(a1_),sin(a1_)}, p2={cos(a2_),sin(a2_)};
+        q1_ = {p1.x+(p2.x-p1.x)*0.2f, p1.y+(p2.y-p1.y)*0.2f};
+
     }
 
 };
@@ -83,10 +108,19 @@ int main() {
     auto plt = ax->line(data);
 
     Robot walle;
-    vector<float>walle_plan_x, walle_plan_y;
-    tie(walle_plan_x, walle_plan_y) = walle.currPlanLine();
-    auto walle_plan_data = create_data().x(walle_plan_x).y(walle_plan_y);
-    auto walle_plan_plt = ax->line(walle_plan_data);
+    for (int i=0; i<20; i++){
+        walle.plan();
+        vector<float>walle_plan_x, walle_plan_y;
+        // tie(walle_plan_x, walle_plan_y) = walle.currPlanLine();
+        // auto walle_plan_data = create_data().x(walle_plan_x).y(walle_plan_y);
+        // auto walle_plan_plt = ax->line(walle_plan_data);
+
+        tie(walle_plan_x, walle_plan_y) = walle.currPlanCurve();
+        auto walle_plan_data = create_data().x(walle_plan_x).y(walle_plan_y);
+        auto walle_plan_plt = ax->line(walle_plan_data);
+        // plt = ax
+        // plt->add_frame(walle_plan_data, i+1);
+    }
 
   // label axis
   ax->xlabel("x");
